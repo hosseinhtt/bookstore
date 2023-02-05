ee12b1ce722877a5f3f4713cd35e639cad0ca6f5
hosseinhtt <42941014+hosseinhtt@users.noreply.github.com>
2023-02-05 22:34:39 +0330

    night1/getPro, updatebadge and fav, add to cart and fav

--- bookstore/Assets/js/js1.js
+++ bookstore/Assets/js/js1.js
@@ -1,3 +1,102 @@
-﻿$(document).ready(){
+﻿$(document).ready(function () {
+   
+    $.post("/Home/getPro")
+        .done(function (res) {
 
+            $("#products").empty();
+
+            for (var item in res) {
+                $("#products").append(
+
+                   
+
+                    '<div class="swiper-slide box">' +
+                    '<div class="icons">' +
+                    '<a  id="' + res[item].pkID + '" class="fas fa-plus" onclick="addtocard(this.id)" href="javascript:void(0)"  ></a>' +
+                    '<a id="' + res[item].pkID + '" href="javascript:void(0)" class="fas fa-heart" onclick="addtofav(this.id)"></a>' +
+                    '<a href="#" class="fas fa-eye"></a>' +
+                    '</div>' +
+                    '<div class="image">' +
+                    ' <img src="/Assets/image/' + res[item].Image +'" />' +
+                    '</div>' +
+                    '<div class="content">' +
+                    '<h3>' + res[item].Name +'</h3 > ' +
+                    '<div class="price"> ' + res[item].Price + " " +' تومان</div>' +
+
+                    '</div>' +
+                    '</div>' +
+
+
+
+                    
+
+                    '<div class="swiper-button-next"></div>' +
+                    '<div class="swiper-button-prev"></div>'
+
+
+
+
+                );
+            }
+        })
+
+
+
+
+
+
+
+});
+
+function addtocard(a) {
+
+
+    var old = localStorage.getItem("basket");
+
+    if (old != null) {
+
+        var neww = old + "," + a;
+    }
+    else {
+
+        neww = a;
+    }
+    localStorage.setItem("basket", neww);
+
+    updatebadge();
+
+}
+
+function updatebadge() {
+
+    var items = localStorage.getItem("basket").split(",");
+
+    document.getElementById("shop_badge").innerHTML = items.length;
+
+}
+
+
+function addtofav(b) {
+
+
+    var old = localStorage.getItem("fav");
+
+    if (old != null) {
+
+        var neww = old + "," + b;
+    }
+    else {
+
+        neww = b;
+    }
+    localStorage.setItem("fav", neww);
+
+    updateFav();
+
+}
+
+function updateFav() {
+    var items = localStorage.getItem("fav").split(",");
+
+    document.getElementById("fav_badge").innerHTML = items.length;
 }
--- bookstore/Controllers/HomeController.cs
+++ bookstore/Controllers/HomeController.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.Mvc;
+using bookstore.Models;
 
 namespace bookstore.Controllers
 {
     public class HomeController : Controller
     {
+        BookStoreEntities context = new BookStoreEntities();
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult getPro()
+        {
+            var res = context.Table_Products.Select(x => new { x.pkID, x.Name, x.Price, x.Image }).ToList();
+
+            return Json(res, JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
